fb730c89e941d00a97be4bce283cb5de768b1e43
Satvik Chachra <50296407+satvikchachra@users.noreply.github.com>
2020-06-19 00:00:29 +0530

    added src

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import marked from 'marked';
+import Footer from './Footer/Footer';
+import './App.css';
+
+class App extends Component {
+
+  initialText = `# Welcome to Marky 🎉
+  
+Marky is an online markdown editor built by [Satvik Chachra](https://github.com/satvikchachra)
+
+  ## 📄 List
+  ### Number List
+  1. One
+  2. Two
+  3. Three
+
+  
+  ### Bulletpoint List
+  * One
+  * Two
+  * Three
+
+  
+  ### Dash also works!
+  - One
+  - Two
+  - Three
+  
+
+  ## 🌃 Images
+  <img src="https://media.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif" height="200px"></img>
+  
+  # 📝 Typography
+  
+  ### Headings
+  
+  # Heading 1
+  ## Heading 2
+  ### Heading 3
+  #### Heading 4
+  ##### Heading 5
+  
+  ### Quotes
+
+> Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Donec odio.Quisque volutpat mattis eros.
+`;
+
+  state = {
+    text: this.initialText
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+
+  render() {
+    const { text } = this.state;
+
+    // Process input text as markdown
+    const markdown = marked(text, { breaks: true });
+
+    // Chnge background-color of text-area
+    const setbg = (color) => {
+      document.querySelector('#editor').style.backgroundColor = `${color}`;
+    }
+
+    return (
+      <div className="App" >
+
+        <div className="header">
+          <h1>Marky</h1>
+          <h3>An online markdown editor built in React.</h3>
+        </div>
+        <div className="container">
+          <div className="editor-container">
+            <textarea id="editor" value={text} onChange={this.handleChange} onFocus={() => setbg('#191919')} onBlur={() => setbg('#121212')}></textarea>
+          </div>
+
+          <div className="preview-container">
+            <div id="preview" dangerouslySetInnerHTML={{ __html: markdown }}></div>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default App;
